5d7abfbf7e2798a2f236b6dcb037806e28ac1107
Rajat <rs20021023@gmail.com>
2022-01-14 16:33:29 +0530

    random color feature done

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 let defaultNUmber = 16;
 var defaultColor = "black";
-let defaultBackgroundColor = "#121212"
+let defaultBackgroundColor = "#121212";
 const container = document.querySelector(".container");
 const resetButton = document.querySelector(".reset-button");
 const colorSelector = document.querySelector(".color-selector");
 const eraserButton = document.querySelector(".eraser-button");
-const pixelRange = document.querySelector('.pixel-range')
-
-
+const pixelRange = document.querySelector(".pixel-range");
+const randomButton = document.querySelector(".random-color");
 
 //EVENTS LISTERNER
 resetButton.addEventListener("click", handleReset);
 colorSelector.addEventListener("change", handleColor);
-eraserButton.addEventListener("click",handleEraser)
-pixelRange.addEventListener("change",handlPixelRange);
-
+eraserButton.addEventListener("click", handleEraser);
+pixelRange.addEventListener("change", handlPixelRange);
+randomButton.addEventListener("click", handlRandomColor);
 
 //FUNCTIONS
 const createGrid = (number) => {
@@ -24,12 +23,11 @@ const createGrid = (number) => {
     container.style.gridTemplateColumns = `repeat(${number},1fr)`;
     container.style.gridTemplateRows = `repeat(${number},1fr)`;
     divSquare.style.backgroundColor = defaultBackgroundColor;
-    divSquare.style.transition = ".3s"
+    divSquare.style.transition = ".3s";
     container.insertAdjacentElement("beforeend", divSquare);
   }
   pixelMaker(defaultColor);
 };
-
 
 function pixelMaker(color) {
   let pixels = container.querySelectorAll("div");
@@ -38,8 +36,7 @@ function pixelMaker(color) {
   });
 }
 
-
-function removePixels(){
+function removePixels() {
   let pixels = container.querySelectorAll("div");
   pixels.forEach((pixel) => {
     pixel.remove();
@@ -48,8 +45,8 @@ function removePixels(){
 
 //EVENT LISTENER FUNCTIONS
 function handleReset() {
-  removePixels()
-  createGrid(pixelRange.value)
+  removePixels();
+  createGrid(pixelRange.value);
 }
 
 const handleHover = (color) => {
@@ -63,19 +60,23 @@ function handleColor(e) {
   pixelMaker(defaultColor);
 }
 
-function handleEraser(){
-  console.log("hello")
+function handleEraser() {
+  console.log("hello");
   let eraserColor = defaultBackgroundColor;
-  pixelMaker(eraserColor)
-
+  pixelMaker(eraserColor);
 }
 
-function handlPixelRange(e){
-  pixelMaker(defaultColor)
-  removePixels()
+function handlPixelRange(e) {
+  pixelMaker(defaultColor);
+  removePixels();
   createGrid(e.target.value);
 }
 
+function handlRandomColor(e) {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  randomColor = "#" + randomColor;
+  pixelMaker(randomColor);
+}
 
 //DEFAULT LAYOUT
 createGrid(defaultNUmber);
